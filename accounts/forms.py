6cb1b4df382dc4f81ad9c4e5e9ab2c50753b503a
shahd1995913 <shahdalkhatib95@gmail.com>
2021-12-12 06:36:03 +0400

    Finish working in  Authentication  in Forms.py

---
+++ accounts/forms.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm
+from django.db import models
+from django.db.models import fields
+from .models import CustomUser
+
+
+class CusotmUserCreationForm(UserCreationForm):
+    class Meta:
+        models=CustomUser
+        fields=("username", "email","password")
+        
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        models=CustomUser
+        fields=("username","email","password")
