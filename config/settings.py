e35cc49443232dd8dfb070050500c5e0030a1d93
Faisal-Kushha <fkushha89@hotmail.com>
2021-12-12 19:34:29 +0200

    feat: Updating the project urls and settings

--- config/settings.py
+++ config/settings.py
@@ -37,12 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    #local
+    # 3rd party
+    "rest_framework",
+    # local
     'accounts',
     'stroke',
     'hepatitis',
-    'result'
+    'result',
     'cancer'
 
 ]
@@ -129,3 +130,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+    ],
+}
