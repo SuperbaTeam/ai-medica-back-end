20d436f0fec326fcb954681cda9167940d519b0e
shahd1995913 <shahdalkhatib95@gmail.com>
2021-12-12 06:30:27 +0400

    finish working in  Authentication  in models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser
+
 from django.db import models
 
-# Create your models here.
+class Account_Info(AbstractUser):  #Person
+
+
+    email_account = models.EmailField(unique=True, max_length=100,verbose_name="Email Address Accunt")
+
+
+    USERNAME_FIELD = 'email_account'
+
+    REQUIRED_FIELDS = ['name', 'mobile','username','password']
+
+    class Meta:
+
+        verbose_name = "Account_Info"
+
+        verbose_name_plural = "accounts"
+
+    def __str__(self) -> str:
+
+        return self.email_account
