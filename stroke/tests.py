63afe2b67d7ba6217cefa7e613c8234fefeee709
Jehadabuawwad <jehadabuawwad@outlook.com>
2021-12-13 20:12:03 +0200

    tests : discover stroke app tests added

--- stroke/tests.py
+++ stroke/tests.py
@@ -87,5 +87,55 @@ class APITest(APITestCase):
                 "owner": test_user.id,
             },
         )
+    def test_create(self):
+
+        test_user = get_user_model().objects.create_user(
+            username="tester", password="pass"
+        )
+        test_user.save()
+
+        test_stroke = Stroke.objects.create(
+            name="Jehad Abu Awwad",
+            email="Jehadabuawwad@outlook.com",
+            mobile=790187612,
+            age=24,
+            bmi=18.0,
+            avg_glucose_level=81.0,
+            gender="True",
+            residence_type="True",
+            ever_married="False",
+            work_type="1, 0, 0, 0, 0",
+            smoking_status="1, 0, 0, 0",
+            hypertension="True",
+            heart_disease="False",
+            status="Positve",
+            owner=test_user,
+        )
+
+        test_stroke.save()
+
+        response = self.client.get(reverse("stroke_create"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.data,
+            {
+                "id": 1,
+                "name": test_stroke.name,
+                "email": test_stroke.email,
+                "mobile": test_stroke.mobile,
+                "age": test_stroke.age,
+                "bmi": test_stroke.bmi,
+                "avg_glucose_level": test_stroke.avg_glucose_level,
+                "gender": test_stroke.gender,
+                "residence_type": test_stroke.residence_type,
+                "ever_married": test_stroke.ever_married,
+                "work_type": test_stroke.work_type,
+                "smoking_status": test_stroke.smoking_status,
+                "hypertension": test_stroke.hypertension,
+                "heart_disease": test_stroke.heart_disease,
+                "status": test_stroke.status,
+                "owner": test_user.id,
+            },
+        )
 
         
