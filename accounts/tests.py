3ad0036d61a18aacf9892b07e72d29e94c0dfd28
shahd1995913 <shahdalkhatib95@gmail.com>
2021-12-14 18:58:13 +0400

    feat:add requrid library  in test

--- accounts/tests.py
+++ accounts/tests.py
@@ -3,18 +3,18 @@ from django.test import TestCase
 # Create your tests here.
 
 from django.contrib.auth import get_user_model
+
 from django.urls import reverse
 
 from django.contrib.auth.models import User
 
 from .models import CustomUser
 
+from .models import Cancer
 
-def setUpTestData(cls):
-        
-        testuser1 = User.objects.create_user(username="test", password="test")
+from rest_framework import status
 
-        testuser1.save()
+from rest_framework.test import APITestCase
 
         
 
