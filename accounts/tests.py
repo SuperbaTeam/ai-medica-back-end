921361e308511ad0b3b7c366331c28810e057812
shahd1995913 <shahdalkhatib95@gmail.com>
2021-12-14 19:00:42 +0400

    feat:add testlist and testdetail methods  in test

--- accounts/tests.py
+++ accounts/tests.py
@@ -70,3 +70,77 @@ class CancerTests(TestCase):
         self.assertEqual(data_cancer.area_mean, 1)
 
         self.assertEqual(data_cancer.smoothness_mean, 1)
+
+class APITest(APITestCase):
+
+    def testlist(self):
+        
+        response = self.client.get(reverse("cancer_list"))
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def testdetail(self):
+
+        user_ = get_user_model().objects.create_user(
+           
+            username="test", password="test"
+        )
+        user_.save()
+
+        testcancer = Cancer.objects.create(
+
+            smoothness_mean=1,
+        
+            compactness_mean=1,
+        
+            concavity_mean=1,
+        
+            concave_points_mean=1,
+        
+            state=1,
+           
+            owner=user_,
+        
+            texture_mean=1,
+        
+            area_mean=1,
+        
+            smoothness_mean=1,
+        
+            compactness_mean=1,
+        
+            concavity_mean=1,
+        
+            concave_points_mean=1,
+        
+            state=1,
+        )
+        testcancer.save()
+
+        response = self.client.get(reverse("cancerdetail", args=[1]))
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+        self.assertEqual(
+         
+            response.data,
+            {
+                "id": 1,
+                
+                "texture_mean": testcancer.texture_mean,
+                
+                "area_mean": testcancer.area_mean,
+                
+                "smoothness_mean": testcancer.smoothness_mean,
+                
+                "compactness_mean": testcancer.compactness_mean,
+                
+                "concavity_mean": testcancer.concavity_mean,
+                
+                "concave_points_mean": testcancer.concave_points_mean,
+                
+                "state": testcancer.state,
+                
+                "owner": user_.id,
+            },
+        )
