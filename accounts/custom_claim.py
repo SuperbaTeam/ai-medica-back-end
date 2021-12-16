86bc03cdf854e7ee647dcb17f0645dff4ce7777a
Jehadabuawwad <jehadabuawwad@outlook.com>
2021-12-16 03:10:17 +0200

    fix : addin crf_exempt

--- accounts/custom_claim.py
+++ accounts/custom_claim.py
@@ -2,9 +2,9 @@ from rest_framework_simplejwt import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 from rest_framework_simplejwt.views import TokenObtainPairView
-
+from django.views.decorators.csrf import csrf_exempt
 class my_token_obtain_per_serializer(TokenObtainPairSerializer):
-    
+    @csrf_exempt
     @classmethod
     def get_token(cls, user):
         token=super().get_token(user)
