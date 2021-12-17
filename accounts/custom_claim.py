2743b9b9ffb41d767b1bfc2b775622183122c994
Jehadabuawwad <jehadabuawwad@outlook.com>
2021-12-17 21:06:30 +0200

    fix : use requeires csrf_token

--- accounts/custom_claim.py
+++ accounts/custom_claim.py
@@ -3,8 +3,8 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 from rest_framework_simplejwt.views import TokenObtainPairView
 from django.views.decorators.csrf import requires_csrf_token
+@requires_csrf_token
 class my_token_obtain_per_serializer(TokenObtainPairSerializer):
-    @requires_csrf_token
     @classmethod
     def get_token(cls, user):
         token=super().get_token(user)
