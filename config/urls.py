fd2b531eaa0aa5e61b181753b9dee7de27b9ccfd
Jehadabuawwad <jehadabuawwad@outlook.com>
2021-12-18 16:08:12 +0200

    feat: register feature added

--- config/urls.py
+++ config/urls.py
@@ -3,14 +3,13 @@ from django.contrib import admin
 
 from django.urls import path, include
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
 )
-from accounts.views import MyObtainTokenPairView
-from accounts.views import  MyObtainTokenPairView
+from accounts.views import MyObtainTokenPairView,RegisterView
 urlpatterns = [
 
     path("api/admin/", admin.site.urls),
+    path('api/register/', RegisterView.as_view(), name='auth_register'),
     path("api/cancer/", include("cancer.urls")),
     path("api/stroke/", include("stroke.urls")),
     path("api/hepatitis/", include("hepatitis.urls")),
